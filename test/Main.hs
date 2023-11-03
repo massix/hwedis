8ae012e2848cbf6066da977ddb71d8204eae0bf6
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2023-11-03 21:18:32 +0100

    tests: implement some more tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 --------------------
 import Comm.Client (Client (Client), Header (Header), extractUserAgent, key, value)
-import Comm.Server (findNextIndex)
+import Comm.Server (Environment, MutableIndex, findNextIndex, runServerStack, serverApp')
+import Control.Concurrent (ThreadId, forkIO, throwTo)
+import Control.Concurrent.STM (atomically, newTVarIO)
+import Control.Monad.Reader (MonadReader (ask), ReaderT (runReaderT))
+import Control.Monad.Trans (lift)
 import Data.Array.IArray (listArray)
+import Data.Array.MArray (MArray (newArray))
 import Data.Configuration (
+  Configuration (getWebserverHost, getWebserverPort),
   ConfigurationSource (..),
   getConfiguration,
+  getRedisHost,
+  getRedisPort,
   readConfiguration,
-  runConfigurationM, Configuration (getWebserverHost, getWebserverPort), getRedisHost, getRedisPort,
+  runConfigurationM,
  )
+import GHC.IO.Exception (FixIOException (FixIOException))
+import Katip (ColorStrategy (..), KatipT, LogEnv, Severity (..), Verbosity (V3), defaultScribeSettings, initLogEnv, logMsg, mkHandleScribe, permitItem, registerScribe, runKatipT)
+import Network.WebSockets (Connection, ControlMessage (..), DataMessage (..), Message (..), defaultConnectionOptions, receive, runClientWith, runServer, send)
+import System.IO (stdout)
 import Test.Tasty (defaultMain)
 import Test.Tasty.HUnit (testCase, (@=?), (@?=))
 import Test.Tasty.Runners (TestTree (TestGroup))
@@ -54,14 +68,71 @@ configurationTest = testCase "Read configuration" $ do
   fromMonad <- runConfigurationM conf (do getConfiguration)
   fromMonad @=? conf
 
-  (redisHost, redisPort) <- runConfigurationM conf (do
-    cnf <- getConfiguration
-    pure (getRedisHost cnf, getRedisPort cnf))
+  (redisHost, redisPort) <-
+    runConfigurationM
+      conf
+      ( do
+          cnf <- getConfiguration
+          pure (getRedisHost cnf, getRedisPort cnf)
+      )
 
   getWebserverHost conf @?= "0.0.0.0"
   getWebserverPort conf @?= 9092
   redisHost @?= "localhost"
   redisPort @?= 6379
+
+type FakeClientM = ReaderT (ThreadId, Connection) (KatipT IO)
+
+runFakeClient :: (ThreadId, Connection) -> LogEnv -> FakeClientM a -> IO a
+runFakeClient t le m = runKatipT le (runReaderT m t)
+
+-- Test the server
+serverTest :: TestTree
+serverTest = testCase "Test Server" $ do
+  let serverHost = "127.0.0.1"
+      serverPort = 9999
+
+  ctx <- atomically $ newArray (0, 20) (Client "undefined" undefined) :: IO Environment
+  idx <- newTVarIO 0 :: IO MutableIndex
+  handleScribe <- mkHandleScribe (ColorLog True) stdout (permitItem DebugS) V3
+  le <- initLogEnv "hwedis" "test" >>= registerScribe "stdout" handleScribe defaultScribeSettings
+
+  runKatipT le $ logMsg "main" DebugS "Starting server on dedicated thread"
+
+  -- Run server on a dedicated light thread
+  threadId <- forkIO $ do
+    runServer serverHost serverPort
+      $ \pc -> runServerStack le (ctx, idx) $ do
+        lift $ lift $ logMsg "main" DebugS "Starting webserver"
+        serverApp' pc
+
+  -- Connect to the server
+  runClientWith serverHost serverPort "/" defaultConnectionOptions [("User-Agent", "Test")] $ \conn -> do
+    runFakeClient (threadId, conn) le mainClient
+
+ where
+  mainClient :: FakeClientM ()
+  mainClient = do
+    ctx <- ask
+    let conn = snd ctx
+        threadId = fst ctx
+
+    liftKatip $ logMsg "client" DebugS "Sending Ping"
+    liftIO $ send conn (ControlMessage (Ping "ping"))
+    msg <- liftIO $ receive conn
+    liftIO $ ControlMessage (Pong "ping") @=? msg
+
+    liftKatip $ logMsg "client" DebugS "Sending Text message"
+    liftIO $ send conn (DataMessage False False False (Text "Hello World" Nothing))
+    msg2 <- liftIO $ receive conn
+    liftIO $ DataMessage False False False (Text "Hello World" Nothing) @=? msg2
+
+    -- Stop the running server
+    liftKatip $ logMsg "client" DebugS "Stopping server"
+    liftIO $ throwTo threadId FixIOException
+
+  liftKatip = lift
+  liftIO = lift . lift
 
 main :: IO ()
 main = defaultMain $ do
@@ -74,6 +145,7 @@ main = defaultMain $ do
         , userAgentTest
         , showHeaderTest
         , findNextIndexTest
+        , serverTest
         ]
     , TestGroup
         "Server"
